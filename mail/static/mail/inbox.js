fbc60b6af00ab537da970340ba0f64f148334059
maryame <elkhalfimaryame@gmail.com>
2024-10-21 16:42:35 +0100

    ?

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -5,11 +5,10 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
-  
-  
+
 
   // By default, load the inbox
-  load_mailbox('inbox')
+  load_mailbox('inbox');
 });
 
 function compose_email() {
