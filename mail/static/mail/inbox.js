fc1ce6de2666029cf9273fd49b0297dc319f1099
maryame <elkhalfimaryame@gmail.com>
2024-10-21 14:51:11 +0100

    add css

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -124,7 +124,7 @@ function load_mailbox(mailbox) {
 document.addEventListener('DOMContentLoaded', function() {
 document.body.addEventListener('click', read_mail);
 function read_mail(event) {
-  
+  const emailElement = event.target.closest('.email');
     const emailId = event.target.id;
     console.log(emailId);
     console.log('_________',array_email,'________')
@@ -134,9 +134,8 @@ function read_mail(event) {
     console.log('Found email:', email);
     if (email){
       email.read=true;
-      email.className="reading";
-   }
-    }}
+      emailElement.style.backgroundColor = "#f0f0f0";
+    }}}
       
   )
 
